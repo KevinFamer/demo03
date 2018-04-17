0203d501171bd4db542fed007df0690be78a2019
zhangdongbo <zdb@qq.com>
2018-04-18 01:30:37 +0800

    提交代码

--- src/game/view/LoadingView.ts
+++ src/game/view/LoadingView.ts
@@ -2,8 +2,21 @@
 * loading界面 
 */
 module Game{
-	export class LoadingView{
-		constructor(){
+	export class LoadingView extends ui.ui_loadingUI {
+
+		constructor() 
+		{
+			super();
+			this.init();
+		}
+
+		init():void 
+		{
+			this.txtProgress.text = "";
+		}
+
+		load():void 
+		{
 
 		}
 	}
